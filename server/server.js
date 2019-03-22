5d4dc2eafa57eba311bd6b2b509fbb8b1d899739
Quy Nguyen Ngoc <quy815069@gmail.com>
2019-03-22 10:35:55 +0700

    update code: build project

--- server/server.js
+++ server/server.js
@@ -5,13 +5,18 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const db = require('./config/config').mongoURI;
-
+const path = require('path');
 app.use(cors());
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 app.use(bodyParser.json({ limit: '100mb', extended: true }));
 
+app.use(express.static('client'))
+app.get('/', (req, res) => {
+  res.status = 200;
+  res.sendFile('index');
+});
 app.use('/api', routes);
-app.get('/', (req, res) => res.send('Hello wolrd'));
+
 
 mongoose
   .connect(db)
